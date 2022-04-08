3bf3e3291ca56fc077b9a854f46da412c58ecb0d
lovenorman <90197758+lovenorman@users.noreply.github.com>
2022-04-08 16:07:02 +0200

    StatusOnIndex

--- BankStartWeb/Pages/Index.cshtml.cs
+++ BankStartWeb/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BankStartWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BankStartWeb.Pages
@@ -6,15 +7,25 @@ namespace BankStartWeb.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        
+        private ApplicationDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
+        public int TotalAmountOfAccounts { get; set; }
+
+        public int TotalAmountOfCustomers { get; set; }
+
+        public int TotalBalance { get; set; }
 
         public void OnGet()
         {
-
+            TotalAmountOfAccounts = _context.Accounts.Count();
+            TotalAmountOfCustomers = _context.Customers.Count();
+            TotalBalance = (int)_context.Accounts.Sum(x => x.Balance);
         }
     }
 }
